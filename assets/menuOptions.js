51d6a408eb3d0b6f9263b8962aa796adc8e01fd1
Agam Damaraju <agamdamaraju@hotmail.com>
2023-05-09 21:11:43 +0530

    First react native commit

---
+++ assets/menuOptions.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    name: 'Controls',
+    iconName: 'remote',
+    href: '/controls',
+  },
+  {
+    name: 'Climate',
+    iconName: 'fan',
+    href: '/climate',
+  },
+  {
+    name: 'Location',
+    iconName: 'map-marker',
+    href: '/location',
+  },
+  {
+    name: 'Summon',
+    iconName: 'ship-wheel',
+    href: '/summon',
+  },
+  {
+    name: 'Security',
+    iconName: 'shield-check',
+    href: '/security',
+  },
+  {
+    name: 'Updates',
+    iconName: 'update',
+    href: '/updates',
+  },
+  {
+    name: 'Pet mode',
+    iconName: 'dog',
+    href: '/pet-mode',
+  },
+  {
+    name: 'Summon',
+    iconName: 'ship-wheel',
+    href: '/summon',
+  },
+  {
+    name: 'Security',
+    iconName: 'shield-check',
+    href: '/security',
+  },
+  {
+    name: 'Updates',
+    iconName: 'update',
+    href: '/updates',
+  },
+];
